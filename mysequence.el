bf5aa31e94d1779527507eb55962185ddd09f33a
Nobuo Tanaka <nobuo.tanaka@mbf.nifty.com>
2019-03-19 14:37:58 +0900

    Update mysequence.el

--- mysequence.el
+++ mysequence.el
@@ -1,4 +1,6 @@
 
+;;; -*- lisp interaction -*-
+
 (setq mylist '(0 1 2 3 4 5 6 7 8 9))
 (0 1 2 3 4 5 6 7 8 9)
 
@@ -11,11 +13,11 @@ mylist
 (nthcdr 1 mylist)
 (1 2 3 4 5 6 7 8 9)
 
-((lambda (seq beg &optional end)
-   (let ((result (nthcdr beg seq)))
-     (if end
-	 (reverse (nthcdr (- (length seq) end)
-			  (reverse result)))
-       result)))
- mylist 1 3)
-(1 2)
+(defun my-subseq (seq beg &optional end)
+  (let ((result (nthcdr beg seq)))
+    (if end
+	(reverse (nthcdr (- (length seq) end)
+			 (reverse result)))
+      result)))
+
+(my-subseq mylist 1 3)
